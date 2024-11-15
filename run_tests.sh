4e0aa18b062b79a5bc6a5b7c2247deac7c0fc5d0
Loi Chyan <loichyan@foxmail.com>
2024-11-15 19:48:22 +0800

    fix: add missing fixes in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-SRC_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-SRC_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 status=0
 for test in "$SRC_DIR"/tests/*; do
