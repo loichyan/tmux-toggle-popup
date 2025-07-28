a553be8e8791f80e770a798a408928f9a827b4dd
Loi Chyan <loichyan@foxmail.com>
2025-07-28 07:21:43 +0000

    test: make it easier to distinguish test runs

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
+set -euo pipefail
 CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 while read -r test; do
-	echo "test: $(basename "$test")"
+	echo "[test] $(basename "$test")"
 	command "$test"
-done < <(find "$CURRENT_DIR/src/" -name "*_tests.sh")
+done < <(find "$CURRENT_DIR/src" -name '*_tests.sh')
--- src/helpers_tests.sh
+++ src/helpers_tests.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2154,SC2120
 
 set -euo pipefail
 CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
@@ -21,6 +20,12 @@ assert_eq() {
 	fi
 }
 
+begin_test() {
+	echo -e "[test] helpers_tests::$1"
+}
+
+#=== test:parse_cmds ===#
+
 test_parse_commands() {
 	parse_cmds "$1"
 	shift
@@ -40,28 +45,33 @@ test_parse_commands() {
 	done
 }
 
-echo "test: delimited_by_semis"
+begin_test "delimited_by_semis"
 test_parse_commands \
 	'set status off ; set exit-empty off' \
 	'set' 'status' 'off' ';' \
 	'set' 'exit-empty' 'off'
-echo "test: delimited_by_line_breaks"
+
+begin_test "delimited_by_line_breaks"
 test_parse_commands \
 	'set status off
 	 set exit-empty off' \
 	'set' 'status' 'off' \
 	'set' 'exit-empty' 'off'
-echo "test: escaped_multiple_commands"
+
+begin_test "escaped_multiple_commands"
 test_parse_commands \
 	'bind -n M-1 display random\ text \\; display and\ more' \
 	'bind' '-n' 'M-1' \
 	'display' 'random text' '\;' \
 	'display' 'and more'
-echo "test: quoted_multiple_commands"
+
+begin_test "quoted_multiple_commands"
 test_parse_commands \
 	"bind -n M-2 \"display 'random text' ; display 'and more'\"" \
 	'bind' '-n' 'M-2' \
 	"display 'random text' ; display 'and more'"
+
+#=== test:interpolate ===#
 
 declare expected output
 test_interpolate() {
@@ -69,35 +79,38 @@ test_interpolate() {
 	assert_eq "$expected" "$output"
 }
 
-echo "test: no_interpolate_of_unknown"
+begin_test "no_interpolate_of_unknown"
 format="{session}/{project}/{popup_name}"
 expected="working/{project}/default"
 test_interpolate session="working" popup_name="default"
 
-echo "test: interpolate_multi"
+begin_test "interpolate_multi"
 format="{var1}/{var2}/{var2}/{var1}"
 expected="value1/value2/value2/value1"
 test_interpolate var1="value1" var2="value2"
 
-echo "test: interpolate_with_equals"
+begin_test "interpolate_with_equals"
 format="{var1}/{var2}/{var2}/{var1}"
 expected="var1=value1/var2=value2/var2=value2/var1=value1"
 test_interpolate var1="var1=value1" var2="var2=value2"
 
+#=== test:batch_get_options ===#
+
 # Simulates a tmux response.
 declare input delimiter="EOF"
+declare var1 var2 var3 var4
 tmux() {
 	printf "%s\nEOF\n" "${input[@]}"
 }
 
-echo "test: batch_get_options"
+begin_test "batch_get_options"
 input=("value1" "value2" "value3")
 batch_get_options var1= var2= var3=
 assert_eq "$var1" "value1"
 assert_eq "$var2" "value2"
 assert_eq "$var3" "value3"
 
-echo "test: batch_get_multiline_options"
+begin_test "batch_get_multiline_options"
 input=(
 	$'\nline1\n'
 	$'line1\nline2'
